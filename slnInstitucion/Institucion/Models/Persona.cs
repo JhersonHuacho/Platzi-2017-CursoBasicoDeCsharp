a8a0db1e317fbe51ae95629425df8e29d4fae818
Jherson <jhersonhuacho@gmail.com>
2019-06-14 22:29:03 -0500

    01: Introducción a la Programación Orientada a Objetos

---
+++ slnInstitucion/Institucion/Models/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Institucion.Models
+{
+    class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public short Edad { get; set; }
+        public string Telefono { get; set; }
+        public string NombreCompleto
+        {
+            get { return $"{Nombre} {Apellido}"; } // string literal
+        }
+    }
+}
--- slnInstitucion/Institucion/Program.cs
+++ slnInstitucion/Institucion/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Institucion.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Institucion
 {
@@ -10,6 +12,30 @@ namespace Institucion
     {
         static void Main(string[] args)
         {
+            WriteLine("GESTION DE INSTITUCION");
+
+            Alumno alumno1 = new Alumno()
+            {
+                Id = 1,
+                Nombre = "Juan Carlos",
+                Apellido = "Ruiz",
+                Edad = 36,
+                Telefono = "944954119",
+                Email = "juank@platzi.com"
+            };
+
+            Persona persona1 = new Persona()
+            {
+                Id = 2,
+                Nombre = "Freddy",
+                Apellido = "Vega",
+                Edad = 86,
+                Telefono = "944954119"                
+            };
+
+            persona1 = alumno1;           
+
+            ReadLine();
         }
     }
 }
